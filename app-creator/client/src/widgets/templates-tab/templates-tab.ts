83be867cbd7fa92cd5f0aa18411dc8b83df39c8f
Mohamed Ibrahim <msibrahim@google.com>
2020-07-28 16:29:58 -0400

    added left drawer template

--- app-creator/client/src/widgets/templates-tab/templates-tab.ts
+++ app-creator/client/src/widgets/templates-tab/templates-tab.ts
@@ -3,6 +3,14 @@
  */
 import { LitElement, html, customElement, css, property } from 'lit-element';
 import { nothing, TemplateResult } from 'lit-html';
+import { onSearchEvent } from '../search-bar/search-bar';
+import { store } from '../../redux/store';
+import { setSelectedTemplate } from '../../redux/actions';
+import { templatesManager } from '../../data/templates';
+import { connect } from 'pwa-helpers';
+import { AppCreatorStore } from '../../redux/reducer';
+import { DeviceType } from '../../redux/types/enums';
+import { classMap } from 'lit-html/directives/class-map';
 import '@polymer/paper-input/paper-input.js';
 import '@polymer/iron-icon/iron-icon.js';
 import '../tab-container/tab-container';
@@ -17,18 +25,15 @@ import '../ui-panel/ui-panel';
 import '../ui-chart/ui-chart';
 import '../search-bar/search-bar';
 import '../empty-notice/empty-notice';
-import { onSearchEvent } from '../search-bar/search-bar';
 import '../template-card/template-card';
-import { store } from '../../redux/store';
-import { setSelectedTemplate } from '../../redux/actions';
-import { templatesManager } from '../../data/templates';
-import { connect } from 'pwa-helpers';
-import { AppCreatorStore } from '../../redux/reducer';
+import '@cwmr/paper-chip/paper-chip.js';
+import { chips } from '../../utils/helpers';
 
 export interface TemplatesTabItem {
   id: string;
   name: string;
   markup: TemplateResult;
+  device: DeviceType;
 }
 
 @customElement('templates-tab')
@@ -42,11 +47,30 @@ export class TemplatesTab extends connect(store)(LitElement) {
     #cards-container {
       margin-top: var(--regular);
     }
+
+    #chips-container {
+      display: flex;
+      flex-wrap: wrap;
+      align-items: center;
+      justify-content: center;
+      margin-top: var(--tight);
+    }
+
+    paper-chip {
+      margin: 0px var(--extra-tight);
+      background-color: var(--primary-color);
+      color: var(--accent-color);
+    }
+
+    .selected-paper-chip {
+      background-color: var(--accent-color);
+      color: var(--primary-color);
+    }
   `;
 
   stateChanged(state: AppCreatorStore) {
-    if (state.template.id !== this.selectedTemplateID) {
-      this.selectedTemplateID = state.template.id;
+    if (state.template.config.parentID !== this.selectedTemplateID) {
+      this.selectedTemplateID = state.template.config.parentID;
       this.requestUpdate();
     }
   }
@@ -61,12 +85,18 @@ export class TemplatesTab extends connect(store)(LitElement) {
    */
   @property({ type: String }) query = '';
 
+  /**
+   * Sets device filter.
+   */
+  @property({ type: String }) deviceFilter = DeviceType.all;
+
   getTemplateCards(showTitle = false) {
     const templates = templatesManager.getTemplates();
-    return templates.map(({ id, name, imageUrl, template }) => {
+    return templates.map(({ id, name, imageUrl, device, template }) => {
       return {
         id,
         name,
+        device,
         markup: html`
           ${showTitle ? nothing : html`<h6 class="subtitle">${name}</h6>`}
           <template-card
@@ -85,15 +115,22 @@ export class TemplatesTab extends connect(store)(LitElement) {
    * Returns widgets with names and ids that include the search query.
    */
   static filterTemplates(
-    templateCards: { id: string; name: string; markup: TemplateResult }[],
-    query: string
+    templateCards: TemplatesTabItem[],
+    query: string,
+    deviceFilter: DeviceType
   ): Array<TemplatesTabItem> {
-    return templateCards.filter(({ id, name }) => {
+    return templateCards.filter(({ id, name, device }) => {
       const lowerCasedQuery = query.toLowerCase();
-      return (
-        id.toLowerCase().includes(lowerCasedQuery) ||
-        name.toLowerCase().includes(lowerCasedQuery)
-      );
+      // Matches either the ID or name.
+      const queryMatch =
+        id.match(new RegExp(`.*${lowerCasedQuery}.*`, 'i')) ||
+        name.match(new RegExp(`.*${lowerCasedQuery}.*`, 'i'));
+
+      // And matches the device type (or all).
+      const deviceMatch =
+        deviceFilter === DeviceType.all || device === deviceFilter;
+
+      return queryMatch && deviceMatch;
     });
   }
 
@@ -112,14 +149,19 @@ export class TemplatesTab extends connect(store)(LitElement) {
     this.query = query.trim();
   }
 
+  handleDeviceFilters(device: DeviceType) {
+    this.deviceFilter = device;
+  }
+
   render() {
-    const { query, getTemplateCards, handleSearch } = this;
+    const { query, deviceFilter, getTemplateCards, handleSearch } = this;
 
     const templateCards = getTemplateCards.call(this);
 
     const filteredTemplates = TemplatesTab.filterTemplates(
       templateCards,
-      query
+      query,
+      deviceFilter
     );
 
     const emptyNotice = html`
@@ -132,12 +174,32 @@ export class TemplatesTab extends connect(store)(LitElement) {
       ></empty-notice>
     `;
 
+    const sortingChips = html`
+      <div id="chips-container">
+        ${chips.map(({ label, device }) => {
+          return html`
+            <paper-chip
+              selectable
+              class=${classMap({
+                'selected-paper-chip': this.deviceFilter === device,
+              })}
+              @click=${() => {
+                this.deviceFilter = device;
+              }}
+              >${label}</paper-chip
+            >
+          `;
+        })}
+      </div>
+    `;
+
     return html`
       <tab-container title="Templates">
         <search-bar
           placeholder="Search for template (ie. side panel)"
           @onsearch=${handleSearch}
         ></search-bar>
+        ${sortingChips}
         <div id="cards-container">
           ${filteredTemplates.map(({ markup }) => markup)}
           ${filteredTemplates.length === 0 ? emptyNotice : nothing}
--- app-creator/client/src/widgets/templates-tab/test/templates-tab_test.ts
+++ app-creator/client/src/widgets/templates-tab/test/templates-tab_test.ts
@@ -3,6 +3,8 @@
  */
 import { TemplatesTab } from '../templates-tab';
 import { fixture, html, expect, assert } from '@open-wc/testing';
+import { DeviceType } from '../../../redux/types/enums';
+import '../templates-tab/templates-tab';
 
 suite('templates-tab', () => {
   test('is defined', () => {
@@ -14,4 +16,56 @@ suite('templates-tab', () => {
     const el = await fixture(html`<templates-tab></templates-tab>`);
     expect(el.shadowRoot!.childNodes.length).to.be.greaterThan(0);
   });
+
+  suite('filter template cards', () => {
+    test('Empty query from all devices', () => {
+      const result = TemplatesTab.filterTemplates(
+        templatesStub,
+        '',
+        DeviceType.all
+      );
+      expect(result.length).to.equal(templatesStub.length);
+    });
+
+    test('Sub query from all devices', () => {
+      const result = TemplatesTab.filterTemplates(
+        templatesStub,
+        'le',
+        DeviceType.all
+      );
+      expect(result.length).to.equal(
+        templatesStub.filter(({ name }) => name.includes('Le')).length
+      );
+    });
+
+    test('Sub query from mobile devices', () => {
+      const result = TemplatesTab.filterTemplates(
+        templatesStub,
+        'le',
+        DeviceType.mobile
+      );
+      expect(result.length).to.equal(1);
+    });
+  });
 });
+
+const templatesStub = [
+  {
+    id: '0',
+    name: 'Left Side Panel',
+    markup: html``,
+    device: DeviceType.desktop,
+  },
+  {
+    id: '1',
+    name: 'Left Drawer Mobile',
+    markup: html``,
+    device: DeviceType.mobile,
+  },
+  {
+    id: '2',
+    name: 'Right Side Panel',
+    markup: html``,
+    device: DeviceType.desktop,
+  },
+];
